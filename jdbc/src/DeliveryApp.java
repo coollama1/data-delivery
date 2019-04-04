fce0570c3c4493329dbe499ce1f143b5ac7c787f
codychen <codychen@codys-mbp.fios-router.home>
2019-04-04 18:45:29 -0400

    Adde

--- jdbc/src/DeliveryApp.java
+++ jdbc/src/DeliveryApp.java
@@ -8,6 +8,8 @@ import javafx.scene.*;
 import javafx.geometry.Insets;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 
 public class DeliveryApp extends Application{
     
@@ -17,13 +19,17 @@ public class DeliveryApp extends Application{
     }
 
     public void start(Stage stage){
-    	stage.setTitle("Login to Account");
+    	stage.setTitle("Data Deliverers Login");
     	
     	GridPane layout = new GridPane();
     	layout.setAlignment(Pos.BASELINE_CENTER);
     	layout.setHgap(10);
     	layout.setVgap(10);
     	layout.setPadding(new Insets(25, 25, 25, 25));
+    	
+    	Text scenetitle = new Text("Welcome to Data Deliverers");
+    	scenetitle.setFont(Font.font("Impact",25));
+    	layout.add(scenetitle, 0, 0, 2, 1);
     	
     	Label userName = new Label("User Name:");
     	layout.add(userName, 0, 1);
@@ -35,13 +41,17 @@ public class DeliveryApp extends Application{
     	TextField pwTextField = new TextField();
     	layout.add(pwTextField, 1, 2);
     	
+    	Button loginBtn = new Button("Login");
+    	HBox lBtn = new HBox(10);
+    	lBtn.setAlignment(Pos.BOTTOM_LEFT);
+    	lBtn.getChildren().add(loginBtn);
+    	layout.add(lBtn, 1, 4);
     	
-    	Button btn = new Button("Login");
-    	HBox hButton = new HBox(10);
-    	hButton.setAlignment(Pos.BOTTOM_RIGHT);
-    	hButton.getChildren().add(btn);
-    	layout.add(hButton, 1, 4);
-    	
+    	Button signUpBtn = new Button("Sign Up");
+    	HBox sBtn = new HBox(10);
+    	sBtn.setAlignment(Pos.BOTTOM_RIGHT);
+    	sBtn.getChildren().add(signUpBtn);
+    	layout.add(sBtn, 1, 4);
     	
     	
     	Scene scene = new Scene(layout, 350, 250);
