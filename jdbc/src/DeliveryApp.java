e64c6ec10761ef9e2eae7dd0601d106d2129416f
codychen <codychen@codys-mbp.fios-router.home>
2019-04-04 23:10:15 -0400

    added more scenes

--- jdbc/src/DeliveryApp.java
+++ jdbc/src/DeliveryApp.java
@@ -12,6 +12,9 @@ import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 
 public class DeliveryApp extends Application{
+	
+	Scene signUpScene;
+	Scene scene1;
     
     public static void main(String [] args){
     	
@@ -19,46 +22,49 @@ public class DeliveryApp extends Application{
     }
 
     public void start(Stage stage){
-    	stage.setTitle("Data Deliverers Login");
+    	stage.setTitle("Data Deliverers");
     	
-    	GridPane layout = new GridPane();
-    	layout.setAlignment(Pos.BASELINE_CENTER);
-    	layout.setHgap(10);
-    	layout.setVgap(10);
-    	layout.setPadding(new Insets(25, 25, 25, 25));
+    	GridPane loginScene = new GridPane();
+    	loginScene.setAlignment(Pos.BASELINE_CENTER);
+    	loginScene.setHgap(10);
+    	loginScene.setVgap(10);
+    	loginScene.setPadding(new Insets(25, 25, 25, 25));
     	
-
     	Text scenetitle = new Text("Welcome to Data Deliverers");
     	scenetitle.setFont(Font.font("Impact",25));
-    	layout.add(scenetitle, 0, 0, 2, 1);
+    	loginScene.add(scenetitle, 0, 0, 2, 1);
     	
-    	Label userName = new Label("User Name:");
-    	layout.add(userName, 0, 1);
+    	Label userName = new Label("Username:");
+    	loginScene.add(userName, 0, 1);
     	TextField userTextField = new TextField();
-    	layout.add(userTextField,1,1);
+    	loginScene.add(userTextField,1,1);
     	
     	Label pw = new Label("Password:");
-    	layout.add(pw, 0, 2);
+    	loginScene.add(pw, 0, 2);
     	TextField pwTextField = new TextField();
-    	layout.add(pwTextField, 1, 2);
+    	loginScene.add(pwTextField, 1, 2);
     	
     	Button loginBtn = new Button("Login");
     	HBox lBtn = new HBox(5);
     	lBtn.setAlignment(Pos.BOTTOM_RIGHT);
     	lBtn.getChildren().add(loginBtn);
-    	layout.add(lBtn, 0, 4);
-    	
+    	loginScene.add(lBtn, 1, 4);
+    	loginBtn.setOnAction(e -> stage.setScene(scene1));
     	
     	Button signUpBtn = new Button("Sign Up");
     	HBox sBtn = new HBox(5);
     	sBtn.setAlignment(Pos.BOTTOM_RIGHT);
     	sBtn.getChildren().add(signUpBtn);
-    	layout.add(sBtn, 1, 4);
+    	loginScene.add(sBtn, 0, 4);
+    	signUpBtn.setOnAction(e -> stage.setScene(signUpScene));
     	
     	
-    	Scene scene = new Scene(layout, 350, 250);
+    	
+    	Scene scene = new Scene(loginScene, 400, 250);
+    	
     	stage.setScene(scene);
     	
         stage.show();
     }   
+    
 }
