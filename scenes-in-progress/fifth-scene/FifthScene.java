c1fc362c24a31fed514095e5e210cb4a6f3d4e4d
coollama1 <lamemarvin@gmail.com>
2019-04-06 23:01:09 -0400

    reformatted DeliveryApp into inner classes

--- scenes-in-progress/fifth-scene/FifthScene.java
+++ scenes-in-progress/fifth-scene/FifthScene.java
@@ -6,6 +6,7 @@ import javafx.scene.text.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.*;
+import javafx.scene.paint.*;
 
 
 public class FifthScene extends Application{
@@ -18,6 +19,7 @@ public class FifthScene extends Application{
     public void start(Stage stage){
         GridPane layout = new GridPane();
         Text trackingTitle = new Text("Enter Tracking Number");
+        Text errorMessage = new Text("Wrong Number");
         TextField trackingTextField = new TextField();
         Button trackingEnterButton = new Button("Enter");
         Button trackingCancleButton = new Button("Cancle");
@@ -25,6 +27,7 @@ public class FifthScene extends Application{
         
         trackingTitle.setFont(new Font("georgia", 20));//arial,courier new, tiems new roman
         trackingTextField.setPrefWidth(100);
+        errorMessage.setFill(Color.CRIMSON);
 
         layout.setAlignment(Pos.BASELINE_CENTER);
         layout.setPadding(new Insets(25,25,25,25));
@@ -36,8 +39,21 @@ public class FifthScene extends Application{
         layout.add(trackingEnterButton,0,2);
         layout.add(trackingCancleButton,1,2);
 
+        trackingEnterButton.setOnAction(e -> {
+            if(trackingTextField.getText().equals("1234567")){
+                trackingTextField.setText("");
+                layout.getChildren().remove(errorMessage);
+            }
+            else{
+                if(!layout.getChildren().contains(errorMessage))
+                    layout.add(errorMessage,2,2);
+            }
+        });
+
         stage.setTitle("Fifth Scene");
         stage.setScene(testingScene);
         stage.show();
     }
 }
+
+        
